c37daf64d7d64e8439e753c69aa1868d39eaef4b
redsamurai73 <mikhail.mikhailov1997@gmail.com>
2020-10-01 23:15:48 +0300

    Fix calc.m

--- main.m
+++ main.m
@@ -6,9 +6,13 @@ disp(greetings);
 
 [h, m, ms, ma] = getArgs();
 fprintf('Параметры:\n');
-fprintf('Толщина среды - %.2f мм\n', h)
-fprintf('Коэффициент экстинкции - %.2f мм^(-1)\n', m)
-fprintf('Коэффициент рассеяния - %.2f мм^(-1)\n', ms)
-fprintf('Коэффициент поглощения - %.2f мм^(-1)\n', ma)
+fprintf('Толщина среды - %.2f м\n', h)
+fprintf('Коэффициент экстинкции - %.2f м^(-1)\n', m)
+fprintf('Коэффициент рассеяния - %.2f м^(-1)\n', ms)
+fprintf('Коэффициент поглощения - %.2f м^(-1)\n', ma)
 
+[t, F_plus] = calc(h, m, ms);
 
+figure(1)
+plot(t, F_plus)
+grid on;
