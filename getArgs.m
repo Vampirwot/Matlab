4d44b3d61e66babf6119e11199b310ac664adb51
Alexander <Vampirwot@yandex.ru>
2020-10-02 11:31:07 +0300

    add Nt and Nm to getArgs

--- getArgs.m
+++ getArgs.m
@@ -1,4 +1,4 @@
-function [h,m,ms,ma] = getArgs()
+function [h,m,ms,ma,Nm,Nt] = getArgs()
 
 state = GetArgsState.Main;
 
@@ -6,6 +6,9 @@ h = 20;
 m = 2.5;
 ms = 1.5;
 ma = m - ms;
+Nm = 100;
+Nt = 1000;
+
 disp("Введите номер пункта");
     while(state ~= GetArgsState.End)
         switch(state)
@@ -28,7 +31,9 @@ disp("Введите номер пункта");
                 fprintf('2) Изменить m: %.2f м^(-1) и ms: %.2f м^(-1)\n', m,ms);
                 fprintf('3) Изменить m: %.2f м^(-1) и ma: %.2f м^(-1)\n', m,ma);
                 fprintf('4) Изменить ms: %.2f м^(-1) и ma: %.2f м^(-1)\n', ms,ma);
-                fprintf('5) Готово\n')
+                fprintf('5) Количество точек для ms и ma: %.d штук\n', Nm);
+                fprintf('6) Количество точек для t: %.d штук\n', Nt);
+                fprintf('7) Готово\n')
                 choise = input("");
                 if(choise == 1)
                     state = GetArgsState.Change_H;
@@ -39,6 +44,10 @@ disp("Введите номер пункта");
                 elseif(choise == 4)
                     state = GetArgsState.Change_Ms_Ma;
                 elseif(choise == 5)
+                    state = GetArgsState.Change_Nm;
+                elseif(choise == 6)
+                    state = GetArgsState.Change_Nt;
+                elseif(choise == 7)
                     state = GetArgsState.End;
                 end
             case GetArgsState.Change_H
@@ -66,8 +75,15 @@ disp("Введите номер пункта");
                 ma = input("");
                 m = ms + ma;
                 state = GetArgsState.Change_Default_Settings;
+             case GetArgsState.Change_Nm
+                fprintf('Введите количество точек для расчета ms и ma\n');
+                Nm = input("");
+                state = GetArgsState.Change_Default_Settings;
+             case GetArgsState.Change_Nt
+                fprintf('Введите количество точек для расчета t\n');
+                Nt = input("");
+                state = GetArgsState.Change_Default_Settings;
         end
     end
 
 end
-
