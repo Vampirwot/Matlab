cf57a8785c79be2b74f6ce92f8b0202682e001b3
Alexander <Vampirwot@yandex.ru>
2020-10-08 13:11:22 +0300

    add
    add ProgressBar class

---
+++ ProgressBar.m
@@ -0,0 +1,35 @@
+classdef ProgressBar
+    
+    properties
+       max_step;
+       reverseStr = "";
+    end
+    
+     properties(Constant)
+       complete = "⬛";
+       incomplete = "⬜";
+    end
+    
+    methods
+        function obj = ProgressBar(max_step)
+            obj.max_step = max_step;
+        end
+        
+        function obj = print(obj,current_step)
+           percentDone = 100 * current_step / obj.max_step;
+           if(percentDone > 100)
+               error("percentDone > 100");
+           end
+           countComplete = floor(percentDone / 10);
+           countInComplete = 10 - countComplete;
+           msgCompleteM = repmat(obj.complete, 1, countComplete);
+           msgInCompleteM = repmat(obj.incomplete, 1, countInComplete);
+           msgInComplete = sprintf('%s', msgInCompleteM);
+           msgComplete = sprintf('%s', msgCompleteM);
+           msg = sprintf('%s%s %3.1f%%\n', msgComplete,msgInComplete,percentDone);
+           fprintf("%s%s",obj.reverseStr, msg);
+           obj.reverseStr = repmat(sprintf('\b'), 1, length(msg));
+        end
+    end
+end
+
--- calcMaMs.m
+++ calcMaMs.m
@@ -3,16 +3,13 @@ function[ma, ms] = calcMaMs(F, Ma, Ms, h)
 ma = Ma(1);
 ms = Ms(1);
 min = intmax;
-stdFMas = zeros(length(Ma),length(Ms));
-
     for i = 1:length(Ma)
         for j = 1:length(Ms)
             M = Ma(i) + Ms(j);
             [t, f] = calc(h, M, Ms(j));
            
             stdF = std(f - F);
-            stdFMas(i,j) = stdF;
-           
+
             if(isnan(stdF))
                continue;
             end
@@ -23,7 +20,5 @@ stdFMas = zeros(length(Ma),length(Ms));
                 ms = Ms(i);
             end
         end
-    end
-    figure(7)
-    surf(Ma,Ms,stdFMas);
+    end 
 end
