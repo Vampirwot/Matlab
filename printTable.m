f396a024613ce1a7472c60fe5133e58f0c46c540
Alexander <Vampirwot@yandex.ru>
2020-10-08 20:52:56 +0300

    delete files

--- printTable.m
+++ printTable.m
@@ -1,13 +1,13 @@
 function printTable(columns,varargin)
 
-   maxLength = getLen(columns);
+   maxLength = strlength(columns);
    for i = 1:length(varargin)
       if(length(columns)~=length(varargin{i})) 
           error('Размеры массив отличаются');
       end
       for j = 1:length(varargin{i})
-          if(maxLength(j) < getLen(varargin{i}(j)))
-            maxLength(j) = getLen(varargin{i}(j));
+          if(maxLength(j) < strlength(varargin{i}(j)))
+            maxLength(j) = strlength(varargin{i}(j));
           end
       end
    end
@@ -15,7 +15,7 @@ function printTable(columns,varargin)
    columnTitle = '| ';
    for i = 1:length(columns)
       border = strcat(border,repmat('-', 1, maxLength(i)+2),'+');
-      columnTitle = strcat(columnTitle, columns{i},string({repmat((' '),1,1+maxLength(i)-getLen(columns{i}))}),' |');
+      columnTitle = strcat(columnTitle, columns{i},string({repmat((' '),1,1+maxLength(i)-strlength(columns{i}))}),' |');
    end
    disp(border);
    disp(columnTitle);
@@ -25,7 +25,7 @@ function printTable(columns,varargin)
    
    for i = 1:length(varargin)
        for j = 1:length(varargin{i})
-         line = strcat(line, varargin{i}(j),string({repmat((' '),1,1+maxLength(j)-getLen(varargin{i}(j)))}),' |');
+         line = strcat(line, varargin{i}(j),string({repmat((' '),1,1+maxLength(j)-strlength(varargin{i}(j)))}),' |');
        end
        disp(line);
        line = '| ';
