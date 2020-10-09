592ac0ae96d41749f91c909dca55ae567d121c2d
redsamurai73 <mikhail.mikhailov1997@gmail.com>
2020-10-09 14:02:59 +0300

    fix printTable.m

--- printTable.m
+++ printTable.m
@@ -15,7 +15,7 @@ function printTable(columns,varargin)
    columnTitle = '| ';
    for i = 1:length(columns)
       border = strcat(border,repmat('-', 1, maxLength(i)+2),'+');
-      columnTitle = strcat(columnTitle, columns{i},string({repmat((' '),1,1+maxLength(i)-getLen(columns{i}))}),' |');
+      columnTitle = strcat(columnTitle, columns{i},char({repmat((' '),1,1+maxLength(i)-getLen(columns{i}))}),' |');
    end
    disp(border);
    disp(columnTitle);
@@ -25,7 +25,7 @@ function printTable(columns,varargin)
    
    for i = 1:length(varargin)
        for j = 1:length(varargin{i})
-         line = strcat(line, varargin{i}(j),string({repmat((' '),1,1+maxLength(j)-getLen(varargin{i}(j)))}),' |');
+         line = strcat(line, varargin{i}(j),char({repmat((' '),1,1+maxLength(j)-getLen(varargin{i}(j)))}),' |');
        end
        disp(line);
        line = '| ';
