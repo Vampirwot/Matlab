6d1db81b6ec5acea7f74b7c84fee2fe62d6c3d89
Alexander <Vampirwot@yandex.ru>
2020-10-15 12:33:38 +0300

    refactor table

--- getArgs.m
+++ getArgs.m
@@ -8,23 +8,22 @@ ms_exp = 0.001;
 ma_exp = m_exp - ms_exp;
 Nma = 20;
 Nms = 20;
-Nt = 10000;
-disp('Введите номер пункта');
+Nt = 1000;
     while(state ~= GetArgsState.End)
         switch(state)
             case GetArgsState.Main
-                disp('1) Использовать настройки по умолчанию');
-                disp('2) Изменить настройки по умолчанию');
-                choise = input('');
-                if(isempty(choise) || choise == 1)
+                disp('Использовать настройки по умолчанию? y/n');
+                choise = input('','s');
+                if(isempty(choise) || choise == 'y')
                     state = GetArgsState.End;
-                elseif(choise == 2)
+                elseif(choise == 'n')
                     state = GetArgsState.Change_Default_Settings;
                 else
                     disp('Ошибка ввода. Попробуйте еще раз.');
                 end
             case GetArgsState.Change_Default_Settings
-                disp(['1) Изменить h: ' num2str(h, 2) ' 1/м']);
+                disp('Введите номер пункта');
+                disp(['1) Изменить h: ' num2str(h, 2) ' м']);
                 disp(['2) Изменить m: ' num2str(m_exp) ' 1/м и ms: ' num2str(ms_exp) ' 1/м']);
                 disp(['3) Изменить m: ' num2str(m_exp) ' 1/м и ma: ' num2str(ma_exp) ' 1/м']);
                 disp(['4) Изменить ms: ' num2str(ms_exp) ' 1/м и ma: ' num2str(ma_exp) ' 1/м']);
@@ -51,28 +50,28 @@ disp('Введите номер пункта');
                     state = GetArgsState.End;
                 end
             case GetArgsState.Change_H
-                disp('Введите h в м');
-                h = input('');
+                disp('Введите h в мм');
+                h = input('') / 1e3;
                 state = GetArgsState.Change_Default_Settings;
             case GetArgsState.Change_M_Ms
-                disp('Введите m в 1/м');
-                m_exp = input('');
-                disp('Введите ms в 1/м');
-                ms_exp = input('');
+                disp('Введите m в 1/мм');
+                m_exp = input('') / 1e3;
+                disp('Введите ms в 1/мм');
+                ms_exp = input('') / 1e3;
                 ma_exp = m_exp - ms_exp;
                 state = GetArgsState.Change_Default_Settings;
             case GetArgsState.Change_M_Ma
-                disp('Введите m в 1/м');
-                m_exp = input('');
-                disp('Введите ma в 1/м');
-                ma_exp = input('');
+                disp('Введите m в 1/мм');
+                m_exp = input('') / 1e3;
+                disp('Введите ma в 1/мм');
+                ma_exp = input('') / 1e3;
                 ms_exp = m_exp - ma_exp;
                 state = GetArgsState.Change_Default_Settings; 
              case GetArgsState.Change_Ms_Ma
-                disp('Введите ms в 1/м');
-                ms_exp = input('');
-                disp('Введите ma в 1/м');
-                ma_exp = input('');
+                disp('Введите ms в 1/мм');
+                ms_exp = input('') / 1e3;
+                disp('Введите ma в 1/мм');
+                ma_exp = input('') / 1e3;
                 m_exp = ms_exp + ma_exp;
                 state = GetArgsState.Change_Default_Settings;
              case GetArgsState.Change_Nms
@@ -89,15 +88,27 @@ disp('Введите номер пункта');
                 state = GetArgsState.Change_Default_Settings;
         end
     end
-  
-    printTable(...
-        {'Параметр','Значение','Ед.измерения'},...
-        {'Толщина среды',num2str(h, 2),'м'},...
-        {'Коэффициент экстинкции',num2str(m_exp, 2),'1/м'},...
-        {'Коэффициент рассеяния',num2str(ms_exp, 2),'1/м'},...
-        {'Коэффициент поглощения',num2str(ma_exp, 2),'1/м'},...
-        {'Количество точек для ms',num2str(Nms),'штук'},...
-        {'Количество точек для ma', num2str(Nma),'штук'},...
-        {'Количество точек для t',num2str(Nt),'штук'});
+    
+ disp('+----------------------------------------------+');
+ disp(['| Параметр                      | ' 'Значение     |']);
+ disp('+----------------------------------------------+');
+ disp([' Толщина среды                    ',num2str(h, '%10.2e') ' м ']);
+ disp([' Коэффициент экстинкции           ',num2str(m_exp, '%-8.2e') ' 1/м ']);
+ disp([' Коэффициент рассеяния            ',num2str(ms_exp, '%-8.2e') ' 1/м ']);
+ disp([' Коэффициент поглощения           ',num2str(ma_exp, '%-8.2e') ' 1/м ']);
+ disp([' Количество точек для ms          ',num2str(Nms,'%-8.2u') ' ']);
+ disp([' Количество точек для ma          ',num2str(Nma,'%-8.2u') ' ']);
+ disp([' Количество точек для t           ',num2str(Nt,'% 8u') ' ']);
+ disp('+----------------------------------------------+');
+ 
+%     printTable(...
+%         {'Параметр','Значение'},...
+%         {'Толщина среды',strcat(num2str(h, 2),' м')},...
+%         {'Коэффициент экстинкции',strcat( num2str(m_exp, 2),' 1/м')},...
+%         {'Коэффициент рассеяния',strcat(num2str(ms_exp, 2),' 1/м')},...
+%         {'Коэффициент поглощения',strcat(num2str(ma_exp, 2),' 1/м')},...
+%         {'Количество точек для ms',num2str(Nms)},...
+%         {'Количество точек для ma', num2str(Nma)},...
+%         {'Количество точек для t',num2str(Nt)});
     
 end
