48647f68d1576723087d3e1589adc5f3052788b7
redsamurai73 <mikhail.mikhailov1997@gmail.com>
2020-10-22 21:08:08 +0300

    Delete Change_M_Ma and Change_M_ms

--- GetArgsState.m
+++ GetArgsState.m
@@ -4,8 +4,6 @@ classdef GetArgsState
         Main,
         Change_H,
         Change_Default_Settings,
-        Change_M_Ms,
-        Change_M_Ma,
         Change_Ms_Ma,
         Change_Nms,
         Change_Nma,
